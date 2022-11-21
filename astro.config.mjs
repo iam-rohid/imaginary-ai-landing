739c42193a7dd6aef8d3f0870bb0fc1256becf0f
Rohid <rohidul209@gmail.com>
2022-11-21 14:45:31 +0600

    basic landing, privacy-policy and terms-of-service pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,17 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://imaginary.rohid.dev",
+  markdown: {
+    remarkPlugins: [],
+  },
+  mdx: {
+    syntaxHighlight: "prism",
+    drafts: true,
+  },
+  integrations: [tailwind(), sitemap(), mdx()],
+});
